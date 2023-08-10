200472f4909136d1ca7c80d57bb0321fb4aa4b63
Ousman Touray <otouray14@gmail.com>
2023-08-10 13:32:59 -0400

    got up to crud functions

--- src/app.js
+++ src/app.js
@@ -4,6 +4,19 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
+app.get('/restaurants', async (req, res) => {
+    const allRestaurants = await Restaurant.findAll({});
+    res.json(allRestaurants);
+});
+
+app.get('/restaurants/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const particularRest = await Restaurant.findByPk(id);
+    res.json(particularRest);
+
+});
+
 
 
 
