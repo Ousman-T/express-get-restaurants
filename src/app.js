c200d66d43fcf6005e95cb8f2ab455250acb4069
Ousman Touray <otouray14@gmail.com>
2023-08-11 10:45:42 -0400

    CRUD operations

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
+app.use(express.json());
+app.use(express.urlencoded());
 
 //TODO: Create your GET Request Route Below: 
 app.get('/restaurants', async (req, res) => {
@@ -16,6 +18,26 @@ app.get('/restaurants/:id', async (req, res) => {
     res.json(particularRest);
 
 });
+
+app.post('/restaurants', async (req, res) => {
+    const newRes = await Restaurant.create({name:'new-food', location: 'NYC', cuisine: 'African'});
+    res.json(newRes);
+});
+
+app.put('/restaurants/:id', async (req, res) => {
+    const currRes = req.params.id;
+    const updateRes = await Restaurant.update({name:'super-new-food'},{where:{id:currRes}});
+    res.json(updateRes);
+
+});
+
+app.delete('/restaurants/:id', async (req, res) => {
+    const pkId = req.params.id;
+    const resToDelete = req.params.body;
+    const deleteRes = await Restaurant.destroy({where:{id:pkId}});
+    res.json(deleteRes);
+
+})
 
 
 
